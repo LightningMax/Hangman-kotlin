9a8beaa44dd571882945b753798e99114d2157cf
Max Reis <maxreis507@gmail.com>
2026-03-13 08:53:58 +0100

    feat: implement input guess verification

--- app/src/main/java/com/example/hangman_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/hangman_kotlin/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.hangman_kotlin
 
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     lateinit var hiddenWordText: TextView
+    lateinit var guessInput: TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,13 +19,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         hiddenWordText = findViewById<TextView>(R.id.display_hidden_word)
+        guessInput = findViewById<TextInputEditText>(R.id.guess_input)
 
         val text = "bonjour"
         var hiddenWord = generateDisplay(text)
-        val letter = 'o'
-        hiddenWord = compare(text, letter, hiddenWord)
 
-        hiddenWordText.text = hiddenWord
+        guessInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEND) {
+
+                val input = guessInput.text.toString()
+
+                if (input.isNotEmpty()) {
+                    val letter = input[0]
+                    hiddenWord = compare(text, letter, hiddenWord)
+                    hiddenWordText.text = hiddenWord
+                }
+
+                true
+            } else {
+                false
+            }
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
